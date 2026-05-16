5f5f5b867c0dff2efcea9badf9ce7fa8170dd8f6
7A <tiadewuyi@gmail.com>
2026-05-16 12:36:16 +0100

    chore: cleaned up config to only retain truly shared types

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,27 +4,16 @@ package config
 const AppName = "dbq"
 const TmpPath = "/var/tmp"
 
+type BinaryAnswer string
+
+const (
+	Yes BinaryAnswer = "Yes"
+	No  BinaryAnswer = "No"
+)
+
 type DatabaseType string
+
 const (
 	Postgres DatabaseType = "postgres"
 	MySQL    DatabaseType = "mysql"
 )
-
-type StorageType string
-const (
-	StorageDirectory StorageType = "directory"
-	StorageCloud     StorageType = "cloud"
-)
-
-type StorageProvider string
-const (
-	S3 StorageProvider = "AWS (S3)"
-	R2 StorageProvider = "Cloudflare R2"
-)
-
-type BinaryAnswer string
-const (
-	Yes BinaryAnswer = "Yes"
-	No BinaryAnswer = "No"
-)
-
